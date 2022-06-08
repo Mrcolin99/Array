e17c0c3fbdec5132245d344dad790f6583532f56
Colin Ciervo <colinciervo@@gmail.com>
2022-06-08 12:34:42 -0400

    Added calls

--- arrays.js
+++ arrays.js
@@ -1,3 +1,11 @@
 var cars = ["WRX", "Crossfire", "Saleen", "Miata"];
 var numbers = [23, 12, 100, 3000000, 2];
 var things = [true, false, true, false];
+
+cars.push();
+numbers.shift();
+things.pop();
+
+console.log(cars);
+console.log(numbers);
+console.log(things);
